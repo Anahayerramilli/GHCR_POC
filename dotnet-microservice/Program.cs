f8e833caf2dccaf551536a2b39735d20371991bb
Yerramilli Ananth <yerramilli.ananth@hcltech.com>
2025-09-19 15:20:12 +0530

    Fix .NET microservice build and update Program.cs and .csproj

--- dotnet-microservice/Program.cs
+++ dotnet-microservice/Program.cs
@@ -1,7 +1,12 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Hosting;
+
 var builder = WebApplication.CreateBuilder(args);
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello from .NET Microservice!");
 
 app.Run();
+
 
